2e07e04e8cd1b908789a097a997fe86e1b3973c1
amitchandakpbi <73231350+amitchandakpbi@users.noreply.github.com>
2025-01-07 20:32:17 +0530

    Learn Power BI 2025 Files
    Learn Power BI 2025 Files

---
+++ Learn Power BI Files/Links.sql
@@ -0,0 +1,29 @@
+Display name: Amit
+Username: powerbi@biworld.in
+Password: J$303704775171um
+Password will be sent to: amit@biworld.in
+
+Apply All Slicers 
+Clear All Slicer 
+
+Create a filter pane using Bookmarks 
+
+
+Links
+https://learn.microsoft.com/en-us/power-bi/fundamentals/desktop-get-the-desktop
+
+https://www.google.com/search?q=download+power+bi&sca_esv=593389473&ei=dO-HZYLaHaGPnesPo-Ky8Ac&ved=0ahUKEwiC5Kys16eDAxWhR2cHHSOxDH4Q4dUDCBA&uact=5&oq=download+power+bi&gs_lp=Egxnd3Mtd2l6LXNlcnAiEWRvd25sb2FkIHBvd2VyIGJpMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADSOUFUABYAHABeAGQAQCYAQCgAQCqAQC4AQPIAQDiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp
+
+https://powerbi.microsoft.com/en-us/downloads/
+https://www.microsoft.com/en-us/download/details.aspx?id=58494
+
+
+
+Inroduction of Panes 
+Also Show Switch between on Object and Build and Visual on right  
+Show move build a visual to pane switcher 
+At one one place call measure driven labels (First time in Bar visual) 
+Example of Measure enabled label 
+example conditional formatting 
+
+
